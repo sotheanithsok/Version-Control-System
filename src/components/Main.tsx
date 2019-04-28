27573fb6261b550c73f7c137c97dfe5e8077dc78
Sotheanith Sok <thetheanith@gmail.com>
2019-04-28 13:20:32 -0700

    Initialize the project

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'
+
+const Main: FunctionComponent = props => {
+    return (
+    <div>
+        <label>This is the main component</label>
+    </div>)
+}
+
+export default Main
