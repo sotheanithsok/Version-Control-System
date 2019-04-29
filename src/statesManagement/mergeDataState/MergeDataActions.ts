f0c750cc8741f7d8397c0dd5298220f79d627d00
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-04-28 18:24:27 -0700

    Add mergeData store (#2)

--- src/statesManagement/mergeDataState/MergeDataActions.ts
+++ src/statesManagement/mergeDataState/MergeDataActions.ts
@@ -1,12 +1,44 @@
+import { MergeDataActionType, IMergeDataState } from './MergeDataTypes';
+import Axios from 'axios';
 import { Dispatch } from 'redux';
-export const mergeIn = () => {
+
+const instance = Axios.create({
+    baseURL: "http://localhost:3001/",
+    timeout: 10000
+})
+export const mergeIn = (sourceDirectory: string, targetDirectory: string, mergeData: IMergeDataState) => {
     return (dispatch: Dispatch, getState: Function) => {
-        
+        instance.post('/mergein', {
+            sourceDirectory: sourceDirectory,
+            targetDirectory: targetDirectory,
+            mergeData: mergeData
+        }).then(response => {
+            if (response.status === 200) {
+                dispatch({
+                    type: MergeDataActionType.UPDATE_MERGEDATA,
+                    mergeData: []
+                })
+            }
+        }).catch(err => {
+            console.log(err)
+        })
     }
 }
 
-export const mergeOut = () => {
+export const mergeOut = (sourceDirectory: string, targetDirectory: string) => {
     return (dispatch: Dispatch, getState: Function) => {
-
+        instance.post('/mergeout', {
+            sourceDirectory: sourceDirectory,
+            targetDirectory: targetDirectory
+        }).then(response => {
+            if (response.status === 200) {
+                dispatch({
+                    type: MergeDataActionType.UPDATE_MERGEDATA,
+                    mergeData: response.data
+                })
+            }
+        }).catch(err => {
+            console.log(err)
+        })
     }
 }
