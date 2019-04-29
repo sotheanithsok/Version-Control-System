15ac520b7118769ac3b8a2fc67641c283c5be34c
Sotheanith Sok <thetheanith@gmail.com>
2019-04-28 22:53:05 -0700

    Succesfully build a modal. Need to implement mergeData

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,26 +1,20 @@
-import React, { FunctionComponent } from "react"
-import { connect } from "react-redux";
-import { getAllManifests, commit } from "../statesManagement/manifestsState/ManifestsActions";
+import React, { FunctionComponent, useRef, createRef } from "react"
 import { Button } from "@material-ui/core";
-import { IStoreStates } from "../statesManagement/Store";
+import MergeModal, { MergeModalHandle } from "./MergeModal";
 
 const TestButton: FunctionComponent = (props: any) => {
+    const mergeModalRef = useRef<any>(null);
 
     return (<div>
+        <MergeModal innerRef={mergeModalRef} value={123}></MergeModal>
         <Button variant="contained" onClick={() => {
-            props.commit("C:\\Users\\Sotheanith Sok\\Desktop\\Test");
+            if (mergeModalRef.current) {
+                mergeModalRef.current.showModal();
+            }
         }}> BUTTON DOES SOMETHING</Button>
-        <pre>{JSON.stringify(props.manifests, null, 2)}</pre>
+
     </div>)
 }
 
-const mapStatesToProp = (store: IStoreStates) => {
-    return {
-        manifests: store.manifestsState
-    }
-}
 
-export default connect(mapStatesToProp, {
-    getAllManifests: getAllManifests,
-    commit:commit
-})(TestButton)
+export default TestButton
