de5c1b397199382d2ed4718960addc68d86cc52c
Yashua Ovando <ovando.yashua@gmail.com>
2019-05-03 19:09:00 -0700

    Able to list manifest list... still need to implement event to open artifact list
    Working on collecting the artifact values from the store... able to list the manifest-list on the side-drawer but to need list artifact list for each manifest item.
    
    Just wanted to push what i had so far

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,5 @@
 import React, { FunctionComponent, useRef, createRef } from 'react'
 import ReactDOM from 'react-dom';
-
-import React, { FunctionComponent, useRef, createRef } from 'react'
 import TestButton from './TestButton';
 import HeaderBar from './HeaderBar'
 import ManiItem from './ManiItem'
@@ -12,20 +10,10 @@ import { any } from 'prop-types';
 import { Button } from '@material-ui/core'
 import MergeModal, { MergeModalHandle } from "./MergeModal";
 import { getAllManifests, commit } from "../statesManagement/manifestsState/ManifestsActions";
-//import {getAllArtifacts, comit } from "../statesManagement/artifactsState/ArtifactsActions"
-import { IStoreStates } from "../statesManagement/Store";
-import { connect } from "react-redux";
-import HeaderBar from './HeaderBar'
-import ManiItem from './ManiItem'
-import ManiList from './ManiList'
-import { any } from 'prop-types';
-import { Button } from '@material-ui/core'
-import MergeModal, { MergeModalHandle } from "./MergeModal";
-import { getAllManifests, commit } from "../statesManagement/manifestsState/ManifestsActions";
 import { IStoreStates } from "../statesManagement/Store";
 import { connect } from "react-redux";
 
-ReactDOM.render(<ArtifList />, document.querySelector('#root'));
+
 
 
 class Main extends React.Component<any, any>
@@ -42,40 +30,11 @@ class Main extends React.Component<any, any>
                 <HeaderBar></HeaderBar>
                 <ManiList/> 
                 <ArtifList/> 
-                <AddButton/>   
-            </div>)
-    }
-
-class Main extends React.Component<any, any>
-{
-    constructor(props: any) {
-        super(props);
-
-    }
-
-
-    render() {
-        return (
-            <div>
-                <HeaderBar></HeaderBar>
-                <ManiList/>     
+                <AddButton/>    
             </div>)
     }
 }
 
-const mapStatesToProp = (store: IStoreStates) => {
-    return {
-        manifests: store.manifestsState,
-    }
-}
-
-
-
-export default connect(mapStatesToProp, {
-    getAllManifests: getAllManifests,
-    
-    commit: commit
-})(Main)
 
 const mapStatesToProp = (store: IStoreStates) => {
     return {
--- src/components/ManiList.tsx
+++ src/components/ManiList.tsx
@@ -7,6 +7,13 @@ import { IStoreStates } from '../statesManagement/Store';
 import { connect } from 'react-redux';
 import { getAllManifests } from '../statesManagement/manifestsState/ManifestsActions';
 
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import FolderIcon from '@material-ui/icons/Folder';
+
 
 
 const styles = (theme: { palette: { background: { paper: any; }; }; }) => ({
@@ -15,6 +22,12 @@ const styles = (theme: { palette: { background: { paper: any; }; }; }) => ({
         maxWidth: 240,
         backgroundColor: theme.palette.background.paper,
     },
+    list: {
+        width: 250
+    },
+    fullList: {
+        width: "auto"
+    },
 });
 
 
@@ -22,28 +35,50 @@ class ManiList extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
         this.state =
-        {
-            side: null,
-        }
+            {
+                side: null,
+            }
     }
 
-    
+    toggleDrawer = (side: any, open: any) => () => {
+        this.setState({
+            [side]: open
+        });
+    };
+
+    openArtList(even:any)
+    {
+
+    }
+
+
 
     render() {
-   
-     
+
+        const manifests = this.props.manifestState
         const { classes } = this.props;
 
-    
+        let manifestList = manifests.map((element: any) => {
+            if(element.tag ===null)
+                return (<ListItem button >{element.id}</ListItem>)
+            else
+                return (<ListItem button >{element.tag}</ListItem>)
+        });
         return (
-            <List className={classes.root}>
-                
+
+            <List className={classes.root} >
+                {manifestList}
             </List>
 
         );
     }
 }
 
+const mapStateToProps = (store: IStoreStates) => {
+    return {
+        manifestState: store.manifestsState
+    }
+}
 
-
-export default (withStyles(styles)(ManiList));
+export default connect(mapStateToProps, {
+})(withStyles(styles)(ManiList));
